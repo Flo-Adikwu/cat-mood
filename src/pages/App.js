25983fa1d8d8a1234c942855550aea9974464a67
Florence <florenceadikwu@gmail.com>
2021-10-12 03:18:42 -0800

    final clean up

--- src/pages/App.js
+++ src/pages/App.js
@@ -67,29 +67,45 @@ const App = () => {
         else setCards(cards);
     }
     return (
-        <div className="container">            <div className="container__firstDiv">                <img src={cat} alt="cat-img" className="container__firstDiv--cat-img" />                <h3 className="container__firstDiv--header-text">What is your cat's<br /> current mood?</h3>                <div className="container__firstDiv--emoji-div">                    <img src={sad}
-            alt="angry-emoji" className="container__firstDiv--emoji" onClick={onClickIndifferent} style={{
-                border: selected && selectedText === "indifferent" ? "1px solid orange" : "1px solid #C4C4C4",
-                backgroundColor: selected && selectedText === "indifferent" ? "rgba(255, 150, 53, 0.2)" : "rgba(175, 170, 168, 0.2)"
-            }}
-        />,
-            <img src={notHavingIt}
-                alt="sad-emoji" className="container__firstDiv--emoji" onClick={onClickSad} style={{
-                    border: selected && selectedText === "sad" ? "1px solid red" : "1px solid #C4C4C4",
-                    backgroundColor: selected && selectedText === "sad" ? "rgba(249, 49, 49, 0.1)" : "rgba(175, 170, 168, 0.2)"
-                }}
-            />,
-            <img src={happy}
-                alt="happy-emoji" className="container__firstDiv--emoji" onClick={onClickHappy} style={{
-                    border: selected && selectedText === "happy" ? "1px solid green" : "1px solid #C4C4C4",
-                    backgroundColor: selected && selectedText === "happy" ? "rgba(50, 194, 82, 0.2)" : "rgba(175, 170, 168, 0.2)"
-                }} />                </div>                {selectedText && <h3 style={textStyle}>{moodText}</h3>}
-            <button disabled={!selected} className={!selected ? "container__firstDiv--disabled" : "container__firstDiv--button"} onClick={save}>Save mood</button>            </div>            <div className="container__secondDiv">                <img src={cat} alt="cat-img" className="container__secondDiv--small-cat-img" /><span><b>Cat mood tracker&trade;</b></span>                <h3 className="container__secondDiv--header-text">MOOD HISTORY</h3>                {/* mapping through constants to display cards */}
-                {saved && <div>                    {cardArr.map((item, i) => (
-                    <div key={i}>{item.value}</div>))}
+        <div className="container">
+            <div className="container__firstDiv">
+                <img src={cat} alt="cat-img" className="container__firstDiv--cat-img" />
+                <h3 className="container__firstDiv--header-text">What is your cat's<br /> current mood?</h3>
+                <div className="container__firstDiv--emoji-div">
+                    <img src={sad}
+                        alt="angry-emoji" className="container__firstDiv--emoji" onClick={onClickIndifferent} style={{
+                            border: selected && selectedText === "indifferent" ? "1px solid orange" : "1px solid #C4C4C4",
+                            backgroundColor: selected && selectedText === "indifferent" ? "rgba(255, 150, 53, 0.2)" : "rgba(175, 170, 168, 0.2)"
+                        }}
+                    />,
+                    <img src={notHavingIt}
+                        alt="sad-emoji" className="container__firstDiv--emoji" onClick={onClickSad} style={{
+                            border: selected && selectedText === "sad" ? "1px solid red" : "1px solid #C4C4C4",
+                            backgroundColor: selected && selectedText === "sad" ? "rgba(249, 49, 49, 0.1)" : "rgba(175, 170, 168, 0.2)"
+                        }}
+                    />,
+                    <img src={happy}
+                        alt="happy-emoji" className="container__firstDiv--emoji" onClick={onClickHappy} style={{
+                            border: selected && selectedText === "happy" ? "1px solid green" : "1px solid #C4C4C4",
+                            backgroundColor: selected && selectedText === "happy" ? "rgba(50, 194, 82, 0.2)" : "rgba(175, 170, 168, 0.2)"
+                        }} />                </div>                {selectedText && <h3 style={textStyle}>{moodText}</h3>}
+                <button disabled={!selected} className={!selected ? "container__firstDiv--disabled" : "container__firstDiv--button"} onClick={save}>Save mood</button>
+            </div>
+            <div className="container__secondDiv">
+                <img src={cat} alt="cat-img" className="container__secondDiv--small-cat-img" /><span><b>Cat mood tracker&trade;</b></span>
+                <h3 className="container__secondDiv--header-text">MOOD HISTORY</h3>
+                {/* mapping through constants to display cards */}
+                {saved && <div>
+                    {cardArr.map((item, i) => (
+                        <div key={i}>{item.value}</div>))}
                 </div>}
                 {/* empty state when no emoji has been selected */}
-                {!saved && <div className="container__secondDiv--empty">                    <img src={empty} alt="cat-empty" />                    <p>No mood history to show yet</p>                </div>}
-            </div>        </div >)
+                {!saved && <div className="container__secondDiv--empty">
+                    <img src={empty} alt="cat-empty" />
+                    <p>No mood history to show yet</p>
+                </div>}
+            </div>
+        </div >
+    )
 }
 export default App;
